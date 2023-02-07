daabf3c386f3aebec8fbfdad5eb7bdd400c273af
Lucas Barbosa Cesar de Oliveira <lucascoliveira819@gmail.com>
2023-02-07 19:03:56 -0400

    Virtualização

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,5 +1,5 @@
-import { useMemo } from "react"
 import { ProductItem } from "./ProductItem"
+import { List, ListRowRenderer } from 'react-virtualized'
 
 interface SearchResultsProps {
   totalPrice: number
@@ -14,13 +14,32 @@ interface SearchResultsProps {
 
 export function SearchResults({ totalPrice, results, onAddToWishist }: SearchResultsProps) {
 
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishist={onAddToWishist}
+        />
+      </div>
+    )
+  }
 
 
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map(product => (<ProductItem key={product.id} product={product} onAddToWishist={onAddToWishist} />))}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+
+      {/* {results.map(product => (<ProductItem key={product.id} product={product} onAddToWishist={onAddToWishist} />))} */}
     </div>
   )
 }
